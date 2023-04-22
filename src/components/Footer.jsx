856eb0bdb2074353325fd937aed43d7669f4bd3f
Akshat Rajput <61741740+akshatrajput58@users.noreply.github.com>
2023-04-22 12:07:44 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="py-4">
+      <div className="container mx-auto px-4">
+        <p className="text-gray-700 text-center"><a href="https://github.com/akshatrajput58">source code</a> | credit to <a href="https://www.youtube.com/@javascriptmastery/videos">Javascript Mastery.</a></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
